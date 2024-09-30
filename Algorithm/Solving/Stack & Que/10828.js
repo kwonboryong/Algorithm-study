c5a90d38aa7dc33c038e56dce8ef7976434326cf
kwonboryong <kwonboryong@naver.com>
2024-09-30 14:46:58 +0900

    Update: 문제풀이 2차 수정

--- Algorithm/Solving/Stack & Que/10828.js
+++ Algorithm/Solving/Stack & Que/10828.js
@@ -1,69 +1,46 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require('fs');
+const path = require('path');
 
-class Stack {
-  constructor() {
-    this.items = [];
-  }
+const input = fs.readFileSync(path.join(__dirname, 'input.txt')).toString().trim().split('\n');
 
-  push(item) {
-    this.items.push(item);
-  }
 
-  pop() {
-    return this.items.pop() || -1; // 비어있으면 -1 반환
-  }
+const stack = [];
+const result = [];
 
-  top() {
-    return this.items.length ? this.items[this.items.length - 1] : -1; // 비어있으면 -1 반환
-  }
+const len = parseInt(input[0]); 
+// 입력 파일(input.txt)의 첫 번째 줄의 명령어 개수
 
-  size() {
-    return this.items.length;
-  }
+for (let i = 1; i <= len; i++) {
+  const command = input[i].trim().split(' ');
+  // 명령어 나누기 (ex. ['push', '3'])
 
-  empty() {
-    return this.items.length === 0 ? 1 : 0; // 비어있으면 1, 아니면 0 반환
+  switch (command[0]) {
+    case 'push':
+      stack.push(parseInt(command[1])); 
+      // 명령어의 숫자를 숫자로 변환해서 스택에 저장
+      break;
+
+    case 'pop':
+      result.push(stack.pop() || -1); 
+      // pop 결과 저장 || 빈 배열이면 -1
+      break;
+
+    case 'size':
+      result.push(stack.length); 
+      // 현재 스택 크기의 크기
+      break;
+
+    case 'empty':
+      result.push(stack.length === 0 ? 1 : 0); 
+      // 빈 배열이면 1(true) || 아니면 0(false)
+      break;
+
+    case 'top':
+      result.push(stack.length ? stack[stack.length - 1] : -1); 
+      // 안 비어있으면 스택의 끝 요소 저장 || 비어있으면 -1 
+      break;
   }
 }
 
-let input = [];
-const stack = new Stack();
-const result = [];
-
-readline.on('line', function(line) {
-  input.push(line);
-}).on('close', function() {
-  const len = parseInt(input[0]); // 첫 번째 줄의 명령 개수
-
-  for (let i = 1; i <= len; i++) {
-    const command = input[i].trim().split(' ');
-
-    switch (command[0]) {
-      case 'push':
-        stack.push(parseInt(command[1])); // 숫자로 변환하여 스택에 추가
-        break;
-
-      case 'pop':
-        result.push(stack.pop()); // pop 결과 저장
-        break;
-
-      case 'size':
-        result.push(stack.size()); // 현재 스택 크기
-        break;
-
-      case 'empty':
-        result.push(stack.empty()); // 비어있는지 확인
-        break;
-
-      case 'top':
-        result.push(stack.top()); // 최상위 요소
-        break;
-    }
-  }
-
-  console.log(result.join('\n')); // 결과 출력
-  process.exit();
-});
+console.log(result.join('\n'));
+// 결과를 줄바꿈해서 출력
